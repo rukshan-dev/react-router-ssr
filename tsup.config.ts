7130a2f005fda12028a87f22a028bfea33400f74
Rukshan <ruk.fernando93@gmail.com>
2025-07-12 23:18:03 +0200

    fix: share assets context

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,12 +2,23 @@ import { defineConfig } from "tsup";
 
 export default defineConfig([
   {
-    entry: ["src/index.ts", "src/ssr.loader.ts", "src/express"],
-    splitting: false,
+    entry: [
+      "src/index.ts",
+      "src/ssr.loader.ts",
+      "src/express",
+    ],
+    splitting: true,
     sourcemap: false,
     clean: true,
     dts: true,
     outDir: "./dist",
-    external: ["react","react-dom","express", "react-router", "virtual:repacked/server"],
+    external: [
+      "src/scripts/context.ts",
+      "react",
+      "react-dom",
+      "express",
+      "react-router",
+      "virtual:repacked/server",
+    ],
   },
 ]);
